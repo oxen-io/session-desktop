437c80f237a6cb0353d41d42704e7d5dabd12216
Audric Ackermann <audric@loki.network>
2020-03-11 14:39:33 +1100

    [test] add send message to open group test

--- integration_test/common.js
+++ integration_test/common.js
@@ -5,58 +5,60 @@ const path = require('path');
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 const RegistrationPage = require('./page-objects/registration.page');
+const ConversationPage = require('./page-objects/conversation.page');
 
 chai.should();
 chai.use(chaiAsPromised);
 
 module.exports = {
-    TEST_MNEMONIC: 'onboard refer gumball nudged hope doctor saucepan wise karate sensible saga tutor doctor',
-    TEST_DISPLAY_NAME: 'test1234',
-    VALID_GROUP_URL: 'https://chat.getsession.org',
-    VALID_GROUP_URL2: 'https://chat-dev.lokinet.org',
-    VALID_GROUP_NAME: 'Session Public Chat',
-    VALID_GROUP_NAME2: 'Loki Dev Chat',
+  TEST_MNEMONIC: 'onboard refer gumball nudged hope doctor saucepan wise karate sensible saga tutor doctor',
+  TEST_DISPLAY_NAME: 'test1234',
+  VALID_GROUP_URL: 'https://chat.getsession.org',
+  VALID_GROUP_URL2: 'https://chat-dev.lokinet.org',
+  VALID_GROUP_NAME: 'Session Public Chat',
+  VALID_GROUP_NAME2: 'Loki Dev Chat',
+  
 
-    timeout(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    },
+  timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  },
+  
+  async startApp() {
+    const environment = this.getEnvironment();
+
+    const app = new Application({
+      path: path.join(__dirname, '..', 'node_modules', '.bin', 'electron'),
+      args: ['.'],
+      env: {
+        NODE_ENV: environment,
+        ELECTRON_ENABLE_LOGGING: true,
+        ELECTRON_ENABLE_STACK_DUMPING: true,
+      },
+      startTimeout: 5000,
+      requireName: 'electronRequire',
+      // chromeDriverLogPath: '../chromedriverlog.txt'
+    })
     
-    async startApp() {
-        const environment = this.getEnvironment();
+    chaiAsPromised.transferPromiseness = app.transferPromiseness;
 
-        const app = new Application({
-            path: path.join(__dirname, '..', 'node_modules', '.bin', 'electron'),
-            args: ['.'],
-            env: {
-                NODE_ENV: environment,
-                ELECTRON_ENABLE_LOGGING: true,
-                ELECTRON_ENABLE_STACK_DUMPING: true,
-            },
-            startTimeout: 5000,
-            requireName: 'electronRequire',
-            // chromeDriverLogPath: '../chromedriverlog.txt'
-        })
-        
-        chaiAsPromised.transferPromiseness = app.transferPromiseness;
-
-        app.start();
-        return app;
+    app.start();
+    return app;
   },
 
-    stopApp(app) {
-        if (app && app.isRunning()) {
-            app.stop()
-                .catch(e => {
-                    // eslint-disable-next-line no-console
-                    console.warn('error:', e);
-            })
+  stopApp(app) {
+    if (app && app.isRunning()) {
+      app.stop()
+        .catch(e => {
+          // eslint-disable-next-line no-console
+          console.warn('error:', e);
+      })
 
     }
     return Promise.resolve();
-    },
-    
-    getEnvironment() {
-        return 'test-integration-session';
+  },
+  
+  getEnvironment() {
+    return 'test-integration-session';
   },
     
   async restoreFromMnemonic(app, mnemonic, displayName) {
@@ -67,6 +69,7 @@ module.exports = {
 
     await app.client.element(RegistrationPage.continueSessionButton).click();
     await app.client.waitForExist(RegistrationPage.conversationListContainer, 4000);
-  },
-
-};
+    },
+  
+    generateSendMessageText: () => `Test message from integration tests ${Date.now()}`,
+}
--- integration_test/open_group_test.js
+++ integration_test/open_group_test.js
@@ -1,22 +1,22 @@
 
 /* eslint-disable func-names  */
 /* eslint-disable import/no-extraneous-dependencies */
-const hooks = require('./common');
-const { after, afterEach, before, beforeEach, describe, it } = require('mocha');
+const commonTest = require('./common');
+const { after, before, describe, it } = require('mocha');
 const ConversationPage = require('./page-objects/conversation.page');
 const RegistrationPage = require('./page-objects/registration.page');
 
 
-describe('Joining open groups', function () {
+describe('Open groups', function () {
   let app;
   this.timeout(20000);
 
   before(async () => {
-    app = await hooks.startApp();
-    await hooks.timeout(2000);
+    app = await commonTest.startApp();
+    await commonTest.timeout(2000);
     await app.client.waitForExist(RegistrationPage.registrationTabs, 4000);
-    await hooks.restoreFromMnemonic(app, hooks.TEST_MNEMONIC, hooks.TEST_DISPLAY_NAME);
-    await hooks.timeout(2000);
+    await commonTest.restoreFromMnemonic(app, commonTest.TEST_MNEMONIC, commonTest.TEST_DISPLAY_NAME);
+    await commonTest.timeout(2000);
 
   });
 
@@ -24,17 +24,17 @@ describe('Joining open groups', function () {
     // eslint-disable-next-line prefer-destructuring
     const ipcRenderer = app.electron.ipcRenderer;
     ipcRenderer.send('delete-all-data');
-    await hooks.timeout(2000);
-    await hooks.stopApp(app);
-    await hooks.timeout(2000);
+    await commonTest.timeout(2000);
+    await commonTest.stopApp(app);
+    await commonTest.timeout(2000);
   });
   
   it('works with valid group url', async () => {
     await app.client.element(ConversationPage.globeButtonSection).click();
     await app.client.element(ConversationPage.joinOpenGroupButton).click();
 
-    await app.client.element(ConversationPage.openGroupInputUrl).setValue(hooks.VALID_GROUP_URL);
-    await app.client.element(ConversationPage.openGroupInputUrl).getValue().should.eventually.equal(hooks.VALID_GROUP_URL);
+    await app.client.element(ConversationPage.openGroupInputUrl).setValue(commonTest.VALID_GROUP_URL);
+    await app.client.element(ConversationPage.openGroupInputUrl).getValue().should.eventually.equal(commonTest.VALID_GROUP_URL);
     await app.client.element(ConversationPage.joinOpenGroupButton).click();
 
     // validate session loader is shown
@@ -45,24 +45,24 @@ describe('Joining open groups', function () {
     await app.client.isExisting(ConversationPage.leftPaneOverlay).should.eventually.be.equal(false);
 
     // validate open chat has been added
-    await app.client.waitForExist(ConversationPage.rowOpenGroupConversationName(hooks.VALID_GROUP_NAME), 4000);
-    await hooks.timeout(1000);
+    await app.client.waitForExist(ConversationPage.rowOpenGroupConversationName(commonTest.VALID_GROUP_NAME), 4000);
+    await commonTest.timeout(1000);
   });
 
   it('cannot join two times the same open group', async () => {
     await app.client.element(ConversationPage.globeButtonSection).click();
     await app.client.element(ConversationPage.joinOpenGroupButton).click();
 
-    await app.client.element(ConversationPage.openGroupInputUrl).setValue(hooks.VALID_GROUP_URL2);
+    await app.client.element(ConversationPage.openGroupInputUrl).setValue(commonTest.VALID_GROUP_URL2);
     await app.client.element(ConversationPage.joinOpenGroupButton).click();
     // first add is a success
-    await app.client.waitForExist(ConversationPage.rowOpenGroupConversationName(hooks.VALID_GROUP_NAME2), 6000);
+    await app.client.waitForExist(ConversationPage.rowOpenGroupConversationName(commonTest.VALID_GROUP_NAME2), 6000);
 
     // adding a second time the same open group
     await app.client.element(ConversationPage.globeButtonSection).click();
     await app.client.element(ConversationPage.joinOpenGroupButton).click();
 
-    await app.client.element(ConversationPage.openGroupInputUrl).setValue(hooks.VALID_GROUP_URL2);
+    await app.client.element(ConversationPage.openGroupInputUrl).setValue(commonTest.VALID_GROUP_URL2);
     await app.client.element(ConversationPage.joinOpenGroupButton).click();
     // validate session loader is not shown
     await app.client.isExisting(ConversationPage.sessionLoader).should.eventually.be.equal(false);
@@ -72,6 +72,30 @@ describe('Joining open groups', function () {
     // validate overlay is still opened
     await app.client.isExisting(ConversationPage.leftPaneOverlay).should.eventually.be.equal(true);
 
-    await hooks.timeout(1000);
+    await commonTest.timeout(1000);
+  });
+
+  it('can send message to open group', async () => {
+    // generate a message containing the current timestamp so we can find it in the list of messages
+    const textMessage = commonTest.generateSendMessageText();
+    // the test 'cannot join two times the same open group' must have been run before that one
+    await app.client.element(ConversationPage.conversationButtonSection).click();
+    
+    await app.client.waitForExist(ConversationPage.rowOpenGroupConversationName(commonTest.VALID_GROUP_NAME2), 200);
+    await app.client.element(ConversationPage.rowOpenGroupConversationName(commonTest.VALID_GROUP_NAME2)).click();
+    await app.client.element(ConversationPage.sendMessageTextarea).setValue(textMessage);
+    await app.client.element(ConversationPage.sendMessageTextarea).getValue().should.eventually.equal(textMessage);
+    // allow some time to fetch some messages
+    await commonTest.timeout(3000);
+
+    // send the message
+    await app.client.keys('Enter');
+    await commonTest.timeout(5000);
+    // validate that the message has been added to the message list view
+    await app.client.waitForExist(ConversationPage.existingSendMessageText(textMessage), 3000);
+    await commonTest.timeout(5000);
+    // we should validate that the message has been added effectively sent 
+    // (checking the check icon on the metadata part of the message?)
+    
   });
 });
