e3329c573158245f1586c7a04107b518f4794941
Emily <emily@oxen.io>
2021-11-24 09:40:50 +1100

    converting tests from javascript to typescript and finishing create user automated test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,8 @@
+import { TestUtils } from "./ts/test/test-utils";
+
 const config = {
-  timeout: 30000,
-  globalTimeout: 600000,
+  timeout: 300000,
+  globalTimeout: 6000000,
   reporter: 'list',
   testDir: './ts/test/automation',
   outputDir: './ts/test/automation/test-results',
