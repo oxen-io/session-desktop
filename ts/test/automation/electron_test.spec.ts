f76b96d51c25d6add2c45aefa6dacb7d5bbe23e4
Emily <emily@oxen.io>
2021-11-24 14:13:31 +1100

    Undoing the code for clear database once test completes

--- ts/test/automation/electron_test.spec.ts
+++ ts/test/automation/electron_test.spec.ts
@@ -1,36 +1,37 @@
 import { _electron, test, expect }  from '@playwright/test';
+// import fs from 'fs-extra';
 
-import pify from 'pify';
-import fs from 'fs';
 
-const { app } = require('electron');
+// import pify from 'pify';
+// import fs from 'fs';
+// import { app } from 'electron';
 
-import userConfig from '../../../app/user_config';
-import ephemeralConfig from '../../../app/ephemeral_config';
-import sql from '../../../app/sql';
+// import userConfig from '../../../app/user_config';
+// import ephemeralConfig from '../../../app/ephemeral_config';
+// import sql from '../../../app/sql';
 
-const getRealPath = pify(fs.realpath);
+// const getRealPath = pify(fs.realpath);
 
-export async function removeDB(appPath: string) {
-  const userDir = await getRealPath(appPath);
-  await sql.removeDB(userDir);
+// export async function removeDB(appPath: string) {
+//   const userDir = await getRealPath(appPath);
+//   await sql.removeDB(userDir);
 
-  try {
-    console.warn('Remove DB: removing.', appPath);
+//   try {
+//     console.warn('Remove DB: removing.', appPath);
 
-    fs.unlink(appPath, () => {
-      // callback
-      console.log("App data cleared");
-      // You should relaunch the app after clearing the app settings.
-      // app.exit();
-    });
+//     fs.unlink(appPath, () => {
+//       // callback
+//       console.log("App data cleared");
+//       // You should relaunch the app after clearing the app settings.
+//       // app.exit();
+//     });
 
-    // userConfig.remove();
-    // ephemeralConfig.remove();
-  } catch (e) {
-    console.warn('Remove DB: Failed to remove configs.', e);
-  }
-}
+//     userConfig.remove();
+//     ephemeralConfig.remove();
+//   } catch (e) {
+//     console.warn('Remove DB: Failed to remove configs.', e);
+//   }
+// }
 
 test('Create User', async() => {
   // Launch Electron app.
@@ -61,15 +62,13 @@ test('Create User', async() => {
     // Exit profile module
     await window.click('.session-icon-button.small');
     // Cleanup device 
-    // await window.click('[data-testid=settings-section]');
-    // await window.click('text=Clear All Data');
-    // await window.click('text=Device Only');
-    // await window.click('text=I am sure');
-    // Wait for data to delete
+    await window.click('[data-testid=settings-section]');
+    await window.click('text=Clear All Data');
+    await window.click('text=Device Only');
+    await window.click('text=I am sure');
+    // Wait for data to delete 
 
-    await removeDB(appPath);
-
-    await window.waitForTimeout(1000);
+    await window.waitForTimeout(10000);
   });
 
 
