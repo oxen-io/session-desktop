818a0f4c1a7f1c7e00c5f29c284b988418869afb
Emily <emily@oxen.io>
2021-11-26 12:15:23 +1100

    fixes user object for new user function

--- ts/test/automation/new_user.js
+++ ts/test/automation/new_user.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.newUser = void 0;
-const newUser = async (window) => {
+const newUser = async (window, userName) => {
     // Create User
     await window.click('text=Create Session ID');
     // Wait for animation for finish creating ID 
@@ -10,10 +10,9 @@ const newUser = async (window) => {
     const sessionid = await window.inputValue('.session-id-editable-textarea');
     await window.click('text=Continue');
     // Input username = testuser
-    const userName = 'testuser';
     await window.fill('#session-input-floating-label', userName);
     await window.click('text=Get Started');
-    return userName, sessionid;
+    return { userName, sessionid };
 };
 exports.newUser = newUser;
 //# sourceMappingURL=new_user.js.map
