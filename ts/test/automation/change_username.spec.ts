4b82244bf5b4291120a1064c6dae318f042c98bc
Emily <emily@oxen.io>
2022-02-07 16:59:55 +1100

    change username test

---
+++ ts/test/automation/change_username.spec.ts
@@ -0,0 +1,33 @@
+import { _electron, expect, Page, test } from '@playwright/test';
+import { newUser } from './new_user';
+import { openApp } from './open';
+import { cleanUpOtherTest, forceCloseAllWindows } from './beforeEach';
+let window: Page | undefined;
+
+test.beforeEach(cleanUpOtherTest);
+test.afterEach(async () => {
+  if (window) {
+    await forceCloseAllWindows([window]);
+  }
+});
+
+test('Change username', async () => {
+  // Open App
+  window = await openApp('1');
+  // Create user
+  await newUser(window, 'userA');
+  // Open Profile
+  await window.click('[data-testid=leftpane-primary-avatar]');
+  // Click on current username to open edit field
+  await window.click('.session-icon-button.medium');
+  // Type in new username
+  await window.fill('.profile-name-input', 'new username');
+  // Save
+  await window.click("'Save'");
+  // Wait for Copy button to appear to verify username change
+  await window.isVisible("'Copy'");
+  // verify name change
+  expect(await window.innerText('[data-testid=your-profile-name]')).toBe('new username');
+  // Exit profile module
+  await window.click('.session-icon-button.small');
+});
