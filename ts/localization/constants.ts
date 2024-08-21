3167f91c9d51c5a296676ee425c0fba41c9cff08
mpretty-cyro <15862619+mpretty-cyro@users.noreply.github.com>
2024-08-21 00:11:54 +0000

    [Automated] Update translations from Crowdin

---
+++ ts/localization/constants.ts
@@ -0,0 +1,10 @@
+export enum LOCALE_DEFAULTS {
+  token_name_short = 'SENT',
+  token_name_long = 'Session Token',
+  token_name_long_plural = 'Session Tokens',
+  ethereum = 'Ethereum',
+  network_name = 'Session Network',
+  app_name = 'Session',
+  arbitrum = 'Arbitrum',
+  arbiscan = 'Arbiscan',
+}
