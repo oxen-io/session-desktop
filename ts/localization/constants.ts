a94c7f8605388e0b25876565ce627504c5efd514
mpretty-cyro <15862619+mpretty-cyro@users.noreply.github.com>
2024-08-28 23:47:31 +0000

    [Automated] Update translations from Crowdin

---
+++ ts/localization/constants.ts
@@ -0,0 +1,18 @@
+export enum LOCALE_DEFAULTS {
+  token_name_short = 'SENT',
+  token_name_long = 'Session Token',
+  token_name_long_plural = 'Session Tokens',
+  ethereum = 'Ethereum',
+  network_name = 'Session Network',
+  app_name = 'Session',
+  arbitrum = 'Arbitrum',
+  arbiscan = 'Arbiscan',
+  session_download_url = 'https://getsession.org/download',
+  token_bonus_title = 'Token Bonus:',
+  claim_your_tokens = 'Claim Your Tokens',
+  price_data_powered_by = 'Price data powered by CoinGecko',
+  gif = 'GIF',
+}
+
+export const rtlLocales = [{"'ar', 'fa', 'he', 'ps', 'ur'"}] as const;
+
